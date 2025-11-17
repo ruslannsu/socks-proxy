21b4c32abfd9feaf0c34d65cc76faccb3dde1754
ruslan <ruslanone57@gmail.com>
2025-11-17 20:05:08 +0700

    add: interp client request

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -19,25 +19,76 @@ class ProxyServer:
         self._epoll = epoll()
         self._epoll.register(self._server_socket.fileno(), EPOLLIN)
 
-        self._client_sockets: dict[int, socket] = {}
+        self._client_sockets: dict[int, tuple[socket, str]] = {}
 
-    
+
+    def _handle_proxy(self, data: bytes, fd: int):
+        client_socket, status = self._client_sockets[fd]
+        if (status == 'SOCKET_ACCEPTED'):
+            request = self._socks_proto.interpretate_authentication_start_request(data)
+            #cheto otpravit
+            if request['socks_version'] != 5:
+                raise(ValueError)
+            
+            response = []
+            response.append(self._socks_proto.socks_ver)
+            response.append(0)
+
+            print(bytes(response))
+            client_socket.send(bytes(response))
+            
+            self._client_sockets[fd] = (client_socket, 'AUTHORIZED')
+            return
+        if (status == 'AUTHORIZED'):
+            request = self._socks_proto.interpretate_client_request(data)
+
+            if request['socks_version'] != 5:
+                raise(ValueError)
+            
+            print(request)
+            
+
+            
+            
+            
+
+
+            
+            
+
+
+            
+            
+            
+                
+                     
+
+
+                
+        
+        
+
+
     def _handle_event(self, fd: int, event: int) -> None:
         if event & EPOLLIN:
             if fd == self._server_socket_fd:
                 client_socket, addr = self._server_socket.accept()
-                self._client_sockets[client_socket.fileno()] = client_socket
+                self._client_sockets[client_socket.fileno()] = (client_socket, 'SOCKET_ACCEPTED')
                 client_socket.setblocking(False)
                 self._epoll.register(client_socket.fileno(), EPOLLIN)    
                 return
 
 
-            bytes = self._client_sockets[fd].recv(1024)
-            self._socks_proto.handle_authentication_start_request(bytes)
-            
-            
-            self._client_sockets[fd].send(b'\x05\x00')
+            client_socket, status = self._client_sockets[fd]
+            data = client_socket.recv(1024)
+            try:
+                self._handle_proxy(data, fd)
+            except:
+                print('he')
+                client_socket.close()
+                self._client_sockets.pop(fd)
 
+            
 
             
             
@@ -60,6 +111,8 @@ class ProxyServer:
             print(len(events))
             for fd, event in events:
                 self._handle_event(fd, event)
+            
+            time.sleep(1)
 
 
                 
--- socks_proto/socks.py
+++ socks_proto/socks.py
@@ -2,9 +2,11 @@
 class SocksProtocolInterpreter:
     def __init__(self) -> None:
         self._address_type = {'IPv4': 1, 'DNS': 2, 'IPv6':3}
-        self_aut_methods = {'noauth': 0, 'GSSAPI': 1, 'UP': 2}
+        self._aut_methods = {'noauth': 0, 'GSSAPI': 1, 'UP': 2}
+        self._commands = {'connect': 1}
+        self.socks_ver = 5
 
-    def handle_authentication_start_request(self, request: bytes) -> dict:
+    def interpretate_authentication_start_request(self, request: bytes) -> dict:
         bytes_list = list(request)
         interpreted_request = {}
 
@@ -20,7 +22,24 @@ class SocksProtocolInterpreter:
 
         print(interpreted_request)
         return interpreted_request
+
+    def interpretate_client_request(self, request: bytes) -> dict:
+        print(request)
+        bytes_list = list(request)
+        interpreted_request = {}
+
+        interpreted_request['socks_version'] = bytes_list[0]
+        interpreted_request['command'] = bytes_list[1]
+        interpreted_request['address_type'] = bytes_list[3]
+
+        address = bytes_list[4:8]
+        interpreted_request['address'] = '.'.join([str(x) for x in address])
+        interpreted_request['port'] = bytes_list[9]
         
+        print("bytes len", len(bytes_list))
+        return interpreted_request
+
+
 
 
 
